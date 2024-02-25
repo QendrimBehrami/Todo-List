a82158323f105ebe54936ccb6672017b80020a16
Qendrim Behrami <behrami@in.tum.de>
2024-02-25 09:09:20 +0100

    Initial project setup

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import "./style.css";
+
+console.log("Hello, world from index.ts!");
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Todo List",
+      template: "src/index.html",
+    }),
+  ],
+  devtool: "inline-source-map",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
