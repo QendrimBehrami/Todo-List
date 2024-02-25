b2a106c36abf79692531cdce3c64d6d1fd16abe5
Qendrim Behrami <behrami@in.tum.de>
2024-02-25 18:43:27 +0100

    Implement sidebar

--- src/views/mainView.ts
+++ src/views/mainView.ts
@@ -2,9 +2,29 @@ import "./../../static/style.css";
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faTasks,
+  faCalendarDay,
+  faCalendarWeek,
+  faCheck,
+  faHouse,
+  faList,
+  faClipboardList,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 // Add the icons to the library
-library.add(faGithub);
+library.add(
+  faGithub,
+  faTasks,
+  faCalendarDay,
+  faCalendarWeek,
+  faCheck,
+  faHouse,
+  faList,
+  faClipboardList,
+  faPlus
+);
 
 // Tell FontAwesome to watch the DOM and add the SVGs when it finds icons
 dom.watch();
